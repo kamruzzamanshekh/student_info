16d78b93cd824b45a440048f76dcc0f562bd0b00
kamruzzamanshekh <shekhnstuiit@gmail.com>
2023-07-11 00:39:12 +0600

    Added Project

---
+++ resources/views/backend/createStudentInfo.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create Student Info</title>
+
+</head>
+<body>
+    <form action="">
+        <label for=""></label>
+        <input type="text" value="fName">
+        <label for=""></label>
+        <input type="text" value="lname">
+        <label for=""></label>
+        <input type="email" value="">
+        <label for=""></label>
+        <input type="number" value="">
+    </form>
+</body>
+</html>
