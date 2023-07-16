22bbae543120f30dd04b1e1d678fde9cb3d55734
kamruzzamanshekh <shekhnstuiit@gmail.com>
2023-07-16 11:55:15 +0600

    edited

--- resources/views/frontend/studentInfoView.blade.php
+++ resources/views/frontend/studentInfoView.blade.php
@@ -9,20 +9,34 @@
 <body>
     <p>Welcome</p>
    <button><a href="{{route('student.create')}}">Create Student Account</a></button>
-   <ol>
-    @foreach ($students as $student)
-        <li>{{$student->first_name." ".$student->last_name." ".$student->email." ".$student->phone}}
-            <a href="{{route('student.show',$student->id)}}">Show</a>
-            <a href="{{route('student.edit', $student->id)}}">Edit</a>
-            <form action="{{route('student.delete', $student->id)}}" method="POST">
-                @method('DELETE');
-                @csrf
-                <button type="submit">Delete</button>
-            </form>
+   <table class="table table-dark">
+    <thead>
+        <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Action</td>
+        </tr>
+        @foreach ($students as $student )
+            <tr>
+                <td>{{$student->first_name}}</td>
+                <td>{{$student->last_name}}</td>
+                <td>{{$student->email}}</td>
+                <td>{{$student->phone}}</td>
+                <td>
+                    <a href="{{route('student.show',$student->id)}}">Show</a>
+                    <a href="{{route('student.edit', $student->id)}}">Edit</a>
+                    <form action="{{route('student.delete', $student->id)}}" method="POST">
+                        @method('DELETE');
+                        @csrf
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
 
-        </li>
-
-    @endforeach
-   </ol>
+            </tr>
+        @endforeach
+    </thead>
+   </table>
 </body>
 </html>
